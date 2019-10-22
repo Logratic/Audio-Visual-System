739a3e43301ec4fc3fe9f5229758cec0e9258d06
Important Yogurt <mrska552@gmail.com>
2019-10-22 18:56:01 -0500

    Create main.js

---
+++ Webserver/main.js
@@ -0,0 +1,27 @@
+//Load HTTP module
+const http = require("http");
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
+// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
+// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
+ 
+//ytdl('http://www.youtube.com/watch?v=A02s8omM_hI')
+//  .pipe(fs.createWriteStream('video.flv'));
+
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
