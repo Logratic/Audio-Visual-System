62a76db80fa4485f4dd35f83cb97b40f561176b7
Aidan Jennings <anjennings@wisc.edu>
2019-10-22 19:00:57 -0500

    Update main.js

--- Webserver/main.js
+++ Webserver/main.js
@@ -10,7 +10,7 @@ const ytdl = require('ytdl-core');
 // TypeScript: import ytdl = require('ytdl-core'); with neither of the above
  
 //ytdl('http://www.youtube.com/watch?v=A02s8omM_hI')
-//  .pipe(fs.createWriteStream('video.flv'));
+///  .pipe(fs.createWriteStream('video.flv'));
 
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
