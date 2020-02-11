7ffb190e06af7e8bc787d4bde2f53ab56632bd16
Aidan Jennings <anjennings@wisc.edu>
2020-02-10 18:47:06 -0600

    tabify

--- colorMatrix.py
+++ colorMatrix.py
@@ -3,20 +3,20 @@ from samplebase import SampleBase
 
 
 class colorMatrix(SampleBase):
-    def __init__(self, *args, **kwargs):
-        super(SimpleSquare, self).__init__(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		super(SimpleSquare, self).__init__(*args, **kwargs)
 
-    def run(self):
-        offset_canvas = self.matrix.CreateFrameCanvas()
-        while True:
-            for x in range(0, self.matrix.width):
-                offset_canvas.SetPixel(1, x, 255, 0, 0)
-                
-            offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
+	def run(self):
+		offset_canvas = self.matrix.CreateFrameCanvas()
+		while True:
+			for x in range(0, self.matrix.width):
+				offset_canvas.SetPixel(1, x, 255, 0, 0)
+				
+			offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
 
 # Main function
 if __name__ == "__main__":
-    mat = colorMatrix()
-    if (not mat.process()):
+	mat = colorMatrix()
+	if (not mat.process()):
 mat.print_help()
